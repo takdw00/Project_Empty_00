6d4644b0c6524d552853d5469ae47377902fd25f
Adolp <takdw00@naver.com>
2021-10-12 19:32:26 +0900

    no message

--- Assets/My Folders/05_Scripts/Party/PartyManager.cs
+++ Assets/My Folders/05_Scripts/Party/PartyManager.cs
@@ -5,8 +5,8 @@ using BTSystem;
 
 public class PartyManager : MonoBehaviour
 {
-    GameObject characters;
-    
+
+
     /*
          PartyManager
         -파티에 소속된 캐릭터들을 가지고 있음
@@ -22,40 +22,54 @@ public class PartyManager : MonoBehaviour
         -전체 캐릭터 리스트에서 파티 맴버를 선택 할 수 있다.
         -파티 맴버에 관해 파티 맴버에게 필요한 설정등을 할 수 있다. (전투방침 같은거)
 
-
      */
 
 
+    [SerializeField] CharacterManager []character; // 전체 캐릭터 리스트
+
     // 이후에 CharacterManager 클래스를 만들어서 그 클래스로 캐릭터 관련 스크립트 접근 가능하도록 만들 것
-    [SerializeField] GameObject partyMember_0;
-    [SerializeField] GameObject partyMember_1;
-    [SerializeField] GameObject partyMember_2;
-    [SerializeField] GameObject partyMember_3;
+    [SerializeField] CharacterManager partyMember_1; // UI에서 1번으로 표시 할 것, 시각적인 편의성을 위해 1번이라 표시
+    [SerializeField] CharacterManager partyMember_2;
+    [SerializeField] CharacterManager partyMember_3;
+    [SerializeField] CharacterManager partyMember_4;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        #region Test Code
+
+        #endregion
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        #region Test Code
+        if(Input.GetKey(KeyCode.P))
+        {
+            partyMember_1.BehaviorTree.StartBehavior(BehaviorMode.INPUT);
+        }
+        if (Input.GetKey(KeyCode.O))
+        {
+            partyMember_1.BehaviorTree.StartBehavior(BehaviorMode.STANDARD);
+        }
+        #endregion
     }
 
     #region Inspector Test
 
     /*이후에 입력 받는 매개변수 partyMember 는 UI창의 버튼 등에서 정보를 가져와 함수를 실행하도록 한다. */
     [ContextMenu("StartMode_Input")]
-    void ConvertInputBehavior(GameObject partyMember)
+    void ConvertInputBehavior(/*CharacterManager partyMember*/)
     {
-        partyMember.transform.Find("BT").GetComponent<BehaviorTree>().StartBehavior(BehaviorMode.INPUT);
+        partyMember_1.BehaviorTree.StartBehavior(BehaviorMode.INPUT);
+        //partyMember.BehaviorTree.StartBehavior(BehaviorMode.INPUT);
     }
     [ContextMenu("StartMode_Standard")]
-    void ConvertStandardBehavior(GameObject partyMember)
+    void ConvertStandardBehavior(/*CharacterManager partyMember*/)
     {
-        partyMember.transform.Find("BT").GetComponent<BehaviorTree>().StartBehavior(BehaviorMode.STANDARD);
+        partyMember_1.BehaviorTree.StartBehavior(BehaviorMode.STANDARD);
+        //partyMember.BehaviorTree.StartBehavior(BehaviorMode.STANDARD);
     }
     #endregion
 
